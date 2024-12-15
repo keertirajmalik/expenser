1b6414d1231ab2ec19f69a9915355cda79e42651
Keertiraj Malik <keertirajmalik@icloud.com>
2024-12-15 22:06:02 +0530

    fix: auth handling

--- expenser-ui/src/main.tsx
+++ expenser-ui/src/main.tsx
@@ -5,12 +5,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "@/index.css";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "next-themes";
 import { App } from "@/App";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <Toaster />
       <BrowserRouter>
         <AuthProvider>
